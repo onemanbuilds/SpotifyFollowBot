090f17db37d9f80825de96b4505701bd69e6ea65
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2021-01-21 12:54:22 +0100

    Update close_chrome.py

--- close_chrome.py
+++ close_chrome.py
@@ -1,7 +1,13 @@
 from os import system,name
+from sys import stdout
 
-def SetTitle(title_name:str):
-    system("title {0}".format(title_name))
+def SetTitle(title:str):
+    if name == 'posix':
+        stdout.write(f"\x1b]2;{title}\x07")
+    elif name in ('ce', 'nt', 'dos'):
+        system(f'title {title}')
+    else:
+        stdout.write(f"\x1b]2;{title}\x07")
 
 def clear():
     if name == 'posix':
@@ -11,7 +17,9 @@ def clear():
     else:
         print("\n") * 120
 
-SetTitle('One Man Builds Chrome Killer')
+SetTitle('[One Man Builds Chrome Killer]')
 clear()
 system('color 2 & taskkill /F /IM chrome.exe /T')
+print('')
+print('PRESS ANY KEY TO EXIT...')
 system('pause > nul')
